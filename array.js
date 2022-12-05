897f43ddb3c0ad8e8f24846be2b1348d38853fc5
kudratoffGG <kudratovabdullah@gmail.com>
2022-12-05 21:33:55 +0500

    Array777

--- array.js
+++ array.js
@@ -1,12 +1,24 @@
+//          array-1
+// let n = Number(prompt(`n kiritilsin: `));
+// let arr = [];
+
+// for(i=1; i<=n; i++) {
+//   let k;
+//   if(i%2==1){
+//     k=i;
+//     arr.push(k);
+//   }
+// }
+// console.log(arr);
+
+
+
+//           array-2
 let n = Number(prompt(`n kiritilsin: `));
 let arr = [];
 
 for(i=1; i<=n; i++) {
-  let k;
-  if(i%2==1){
-    k=i;
-    arr.push(k);
-  }
+  k = 2**i;
+  arr.push(k);
 }
 console.log(arr);
-console.log(arr[2]);
